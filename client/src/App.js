5cf0d3ced30746e6352cb5d5f8d4f01d0b62c347
sushanth870 <sushanthkumar870@gmail.com>
2022-05-01 20:29:17 +0530

    books-json file

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,26 @@
-import './App.css';
-import Home from './Screens/Home';
-import BookDetails from './Screens/BookDetails';
-import MyProfile from './Screens/MyProfile';
-import Login from './Screens/LoginScreen'
-import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
+import "./App.css";
+import Home from "./Screens/Home";
+import BookDetails from "./Screens/BookDetails";
+import MyProfile from "./Screens/MyProfile";
+import Login from "./Screens/LoginScreen";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-    {/* {
       <div>
-      <Router>
-      <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/myprofile" component={MyProfile}/>
-          <Route exact path="/bookdetails" component={BookDetails}/>
-        <Switch>
-          </Router>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/myprofile" element={<MyProfile />} />
+            <Route exact path="/bookdetails" element={<BookDetails />} />
+          </Routes>
+        </Router>
       </div>
-    } */}
-    <MyProfile/>
-    {/* <Login/> */}
-    {/* <Home/> */}
-    {/* <BookDetails/> */}
-     
+      {/* <MyProfile/> */}
+      {/* <Login/> */}
+      {/* <Home/> */}
+      {/* <BookDetails/> */}
     </>
   );
 }
